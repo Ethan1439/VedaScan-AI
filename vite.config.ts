7ab19cacdc428151c2e5f09d24d9c02e3c7bdfe8
Ethan1439 <130270806+Ethan1439@users.noreply.github.com>
2026-06-21 04:03:40 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  base: './', // 👈 This automatically sets relative pathing so it won't crash on GitHub Pages
+  plugins: [react()]
 })
