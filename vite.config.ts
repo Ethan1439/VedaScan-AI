13c4b638112038106091ac4484b81b619449bea6
Ethan1439 <130270806+Ethan1439@users.noreply.github.com>
2026-06-21 04:16:02 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './' // 👈 This maps file directories properly so GitHub Pages reads it smoothly
+  base: './'
 })
+
